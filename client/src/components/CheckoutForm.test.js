8940aad9466323931b8f58eb125d91a2f173670c
Greyson <greyson.r.hamilton@gmail.com>
2020-12-13 12:10:41 -0500

    mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { fireEvent, screen, render } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
