ca08639dd8d5ef749e3cfd7ab496ec0647600b36
Greyson <greyson.r.hamilton@gmail.com>
2020-12-13 11:10:30 -0500

    bug fixed on typo

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,9 +1,9 @@
 // write your custom hook here to control your checkout form
 import { useState } from 'react';
 
-export const useForm = (initalValues) => {
+export const useForm = (initialValue) => {
 
-    const [values, setValues] = useState(initialValues);
+    const [values, setValues] = useState(initialValue);
 
     const handleChange = (e) => {
         
